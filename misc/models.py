d212ec9bbcedbb83ef341576d0b724ae2fd34158
PortiQq <Qba12@windowslive.com>
2024-06-03 20:14:18 +0200

    Add: Getting user events
    - /api/users/events - for getting all current user events
    - /api/users/events/<id> - for getting chosen user events

--- misc/models.py
+++ misc/models.py
@@ -21,4 +21,4 @@ class Image(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Image {self.id} uploaded at {self.uploaded_at}"
+        return f"Image {self.id}, {self.file}"
--- user/views.py
+++ user/views.py
@@ -4,9 +4,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from post.models import Post
 from post.serializers import PostSerializer
+from event.models import Event
+from event.serializers import EventSerializer
 from .serializers import UserSerializer
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import AllowAny
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .models import User
 # Create your views here.
@@ -27,7 +31,7 @@ class OwnUserDetailView(APIView):
         serializer = UserSerializer(user)
         return Response(serializer.data)
     
-class PublicUserDetailView(APIView):
+class UserDetailByIdView(APIView):
     permission_classes = [AllowAny]
     def get(self, request, user_id):
         try:
@@ -37,3 +41,29 @@ class PublicUserDetailView(APIView):
 
         serializer = UserSerializer(user)
         return Response(serializer.data)
+    
+class UserPostByIdView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request, user_id):
+        posts = Post.objects.filter(user_id=user_id)
+        serializer = PostSerializer(posts, many=True)
+
+        return Response({'posts': serializer.data})
+    
+class UserEventsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        events = Event.objects.filter(user_id=user)
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class UserEventsByIdView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        events = Event.objects.filter(user_id=user)
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
