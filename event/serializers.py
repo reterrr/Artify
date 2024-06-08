76dbcb30e909d1d44c288483a9ab9791eafb6dde
Michał Mroczka <michal.mroczka03@gmail.com>
2024-06-08 13:30:40 +0200

    Fixed adding images to events

--- event/serializers.py
+++ event/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Event
+from misc.serializers import ImageSerializer
 
 class EventSerializer(ModelSerializer):
+    event_image = ImageSerializer()
+    
     class Meta:
         model = Event
         fields = ('title', 'date', 'description', 'user_id', 'event_image', 'event_categories', 'event_tags')
         # read_only_fields = ['user']
 
 class CreateEventSerializer(ModelSerializer):
+    
     class Meta:
         model = Event
-        fields = ('title', 'date', 'description', 'event_image', 'event_categories', 'event_tags')
+        fields = ('title', 'date', 'description', 'user_id', 'event_image', 'event_categories', 'event_tags')
